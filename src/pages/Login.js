e14c6fd027d1be68840834823376cfc674ae7cfa
S1M0N3Y <88841451+simoney199@users.noreply.github.com>
2024-05-15 08:17:23 +0700

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -72,7 +72,7 @@ const Login = () => {
                             <div className='bg-slate-100 p-2'>
                                 <input 
                                     type='email' 
-                                    placeholder='enter email' 
+                                    placeholder='Nhập email' 
                                     name='email'
                                     value={data.email}
                                     onChange={handleOnChange}
@@ -81,11 +81,11 @@ const Login = () => {
                         </div>
 
                         <div>
-                            <label>Password : </label>
+                            <label>Mật khẩu : </label>
                             <div className='bg-slate-100 p-2 flex'>
                                 <input 
                                     type={showPassword ? "text" : "password"} 
-                                    placeholder='enter password'
+                                    placeholder='Nhập mật khẩu'
                                     value={data.password}
                                     name='password' 
                                     onChange={handleOnChange}
@@ -105,15 +105,15 @@ const Login = () => {
                                 </div>
                             </div>
                             <Link to={'/forgot-password'} className='block w-fit ml-auto hover:underline hover:text-red-600'>
-                                Forgot password ?
+                                Quên mật khẩu?
                             </Link>
                         </div>
 
-                        <button className='bg-red-600 hover:bg-red-700 text-white px-6 py-2 w-full max-w-[150px] rounded-full hover:scale-110 transition-all mx-auto block mt-6'>Login</button>
+                        <button className='bg-red-600 hover:bg-red-700 text-white px-6 py-2 w-full max-w-[150px] rounded-full hover:scale-110 transition-all mx-auto block mt-6'>Đăng nhập</button>
 
                     </form>
 
-                    <p className='my-5'>Don't have account ? <Link to={"/sign-up"} className=' text-red-600 hover:text-red-700 hover:underline'>Sign up</Link></p>
+                    <p className='my-5'>Chưa có tài khoản ?<Link to={"/sign-up"} className=' text-red-600 hover:text-red-700 hover:underline'>Đăng ký</Link></p>
             </div>
 
 
